d063e7d49cb69a9ac329ff62fa8fce1f4fe944c3
Ian Rees <ian@ianrees.net>
2025-03-06 17:51:19 -0800

    Fixes

--- main.go
+++ main.go
@@ -10,6 +10,15 @@ import (
 	"github.com/interline-io/interline-healthcheck/hc"
 )
 
+func getEnvKeys(v ...string) string {
+	for _, key := range v {
+		if os.Getenv(key) != "" {
+			return key
+		}
+	}
+	return ""
+}
+
 func main() {
 	healthcheckId := ""
 	workflowName := ""
@@ -17,12 +26,31 @@ func main() {
 	workflowOk := true
 	workflowSetFail := false
 	workflowSetSuccess := false
-	flag.StringVar(&healthcheckId, "healthcheck-id", os.Getenv("HEALTHCHECK_ID"), "Healthcheck ID, defaults to $HEALTHCHECK_ID")
-	flag.StringVar(&workflowName, "workflow-name", os.Getenv("WORKFLOW_NAME"), "Workflow name, defaults to $WORKFLOW_NAME")
-	flag.StringVar(&workflowStatus, "workflow-status", os.Getenv("WORKFLOW_STATUS"), "Workflow status, defaults to $WORKFLOW_STATUS")
+	flag.StringVar(&healthcheckId, "healthcheck-id", "", "Healthcheck ID, defaults to $HEALTHCHECK_ID or $HEALTHCHECKSIO_CHECK_ID")
+	flag.StringVar(&workflowName, "workflow-name", "", "Workflow name, defaults to $WORKFLOW_NAME")
+	flag.StringVar(&workflowStatus, "workflow-status", "", "Workflow status, defaults to $WORKFLOW_STATUS")
 	flag.BoolVar(&workflowSetFail, "fail", false, "Set fail state")
 	flag.BoolVar(&workflowSetSuccess, "success", false, "Set success state")
 	flag.Parse()
+
+	// Configure healthcheckId
+	if healthcheckId == "" {
+		healthcheckId = getEnvKeys("HEALTHCHECK_ID", "HEALTHCHECKSIO_CHECK_ID")
+	}
+
+	// Configure slack
+	slackUrlSuccess := getEnvKeys("SLACK_URL_SUCCESS", "SLACK_URL_BOTS")
+	slackUrlFailure := getEnvKeys("SLACK_URL_FAILURE", "SLACK_URL_GENERAL")
+
+	// Configure workflowName and workflowStatus
+	if workflowName == "" {
+		workflowStatus = getEnvKeys("WORKFLOW_NAME")
+	}
+	if workflowStatus == "" {
+		workflowStatus = getEnvKeys("WORKFLOW_STATUS")
+	}
+
+	// Configure workflowOk
 	if workflowStatus != "" && strings.ToLower(workflowStatus) != "succeeded" {
 		workflowOk = false
 	}
@@ -32,6 +60,8 @@ func main() {
 	if workflowSetSuccess {
 		workflowOk = true
 	}
+
+	// Debug
 	cmd := flag.Arg(0)
 	fmt.Println(
 		"cmd:", cmd,
@@ -44,11 +74,12 @@ func main() {
 		fail("set --workflow-name or $WORKFLOW_NAME")
 	}
 
+	// Configure to run
 	healthCheck := hc.NewHealthcheck(
 		healthcheckId,
 		workflowName,
-		os.Getenv("SLACK_URL_SUCCESS"),
-		os.Getenv("SLACK_URL_FAILURE"),
+		slackUrlSuccess,
+		slackUrlFailure,
 	)
 
 	// Run subcommand
